4174898a80e11298d0d74537ca7ae480a61ed876
Naman Gupta <naman.gupta507@gmail.com>
2024-06-01 12:16:56 +0530

    changes final

--- src/comp/home.js
+++ src/comp/home.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import './home.css'
+import './home.css';
 import { Link } from 'react-router-dom'
 import Homeproduct from './home_product'
 import { AiFillEye, AiFillHeart, AiOutlineShoppingCart} from "react-icons/ai";
